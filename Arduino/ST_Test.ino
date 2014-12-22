7249c935cb71589e3954d1a8d97c1a0a860270fd
Daniel Ogorchock <djogorchock@gmail.com>
2014-12-22 02:01:25 -0500

    for real this time

---
+++ Arduino/ST_Test.ino
@@ -0,0 +1,18 @@
+#include <Device.h>
+#include <Sensor.h>
+#include <PollingSensor.h>
+
+st::PollingSensor *ps;
+
+
+void setup()
+{
+  st::PollingSensor::DEBUG=true;
+  Serial.begin(9600);
+  ps=new st::PollingSensor("Daniel", 1000);
+}
+
+void loop()
+{
+  ps->update();
+}
