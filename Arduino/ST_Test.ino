3be53a9b551c8ea31dc3707be4328abc5ee974b3
Daniel Ogorchock <djogorchock@gmail.com>
2014-12-23 00:38:16 -0500

    updated example to contain PS_Luminance

--- Arduino/ST_Test.ino
+++ Arduino/ST_Test.ino
@@ -1,9 +1,10 @@
 #include <Device.h>
 #include <InterruptSensor.h>
 #include <PollingSensor.h>
+#include <PS_Illuminance.h>
 #include <Sensor.h>
 
-const unsigned int SENSOR_COUNT=10;
+const unsigned int SENSOR_COUNT=11;
 st::Sensor *sensors[SENSOR_COUNT];
 
 //test sketch
@@ -17,11 +18,12 @@ void setup()
   sensors[2]=new st::PollingSensor("GarageDoor1", 4000);
   sensors[3]=new st::PollingSensor("GarageDoor2", 4000, 2000);
   sensors[4]=new st::PollingSensor("LightSensor", 30000);
-  sensors[5]=new st::InterruptSensor("GarageButton", 2, HIGH);
-  sensors[6]=new st::InterruptSensor("Doorbell", 3, HIGH);
-  sensors[7]=new st::InterruptSensor("Alarm", 4, HIGH);
-  sensors[8]=new st::InterruptSensor("Alert", 5, HIGH);
-  sensors[9]=new st::InterruptSensor("GarbageDisposal", 6, HIGH);
+  sensors[5]=new st::InterruptSensor("GarageButton", 8, HIGH);
+  sensors[6]=new st::InterruptSensor("Doorbell", 9, HIGH);
+  sensors[7]=new st::InterruptSensor("Alarm", 10, HIGH);
+  sensors[8]=new st::InterruptSensor("Alert", 11, HIGH);
+  sensors[9]=new st::InterruptSensor("GarbageDisposal", 12, HIGH);
+  sensors[10]=new st::PS_Illuminance("illuminance", 1000, 0, A5);
 }
 
 void loop()
