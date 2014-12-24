fb58fac7e8904981cf6cca33cef6f77b18967d61
Daniel Ogorchock <djogorchock@gmail.com>
2014-12-23 22:22:03 -0500

    Updated example sketch to utilize the Everything class

--- Arduino/ST_Test.ino
+++ Arduino/ST_Test.ino
@@ -1,11 +1,13 @@
+#include <Constants.h>
 #include <Device.h>
+#include <Everything.h>
+#include <Executor.h>
 #include <InterruptSensor.h>
 #include <PollingSensor.h>
 #include <PS_Illuminance.h>
 #include <Sensor.h>
 
-const unsigned int SENSOR_COUNT=11;
-st::Sensor *sensors[SENSOR_COUNT];
+st::Everything everything;
 
 st::PollingSensor test0("Temperature", 10000);
 st::PollingSensor test1("Humidity", 20000);
@@ -19,28 +21,29 @@ st::PS_Illuminance test7("illuminance", 5000, 0, A5);
 //test sketch
 void setup()
 {
+  Serial.begin(9600); //THIS MUST BE CALLED FIRST TO AVOID TERRIBLE RESULTS
+  
+  st::Everything::debug=true;
   st::Device::debug=true;
   st::PollingSensor::debug=true;
   st::InterruptSensor::debug=true;
-  Serial.begin(9600);
   
-  sensors[0]=(st::Sensor*)&test0; //new st::PollingSensor("Temperature", 10000);
-  sensors[1]=(st::Sensor*)&test1; //new st::PollingSensor("Humidity", 20000);
-  sensors[2]=(st::Sensor*)&test2; //new st::PollingSensor("GarageDoor1", 4000);
-  sensors[3]=(st::Sensor*)&test3; //new st::PollingSensor("GarageDoor2", 4000, 2000);
-  sensors[4]=(st::Sensor*)&test4; //new st::PollingSensor("LightSensor", 30000);
-  sensors[5]=(st::Sensor*)&test5; //new st::InterruptSensor("GarageButton", 8, HIGH);
-  sensors[6]=(st::Sensor*)&test6; //new st::InterruptSensor("Doorbell", 9, HIGH);
-  sensors[7]=(st::Sensor*)&test7; //new st::InterruptSensor("Alarm", 10, HIGH);
-  sensors[8]=new st::InterruptSensor("Alert", 11, HIGH);
-  sensors[9]=new st::InterruptSensor("GarbageDisposal", 12, HIGH);
-  sensors[10]=new st::PollingSensor("dynamicPolling", 2000, 0);
-  }
+  everything.addSensor(&test0);
+  everything.addSensor(&test1); 
+  everything.addSensor(&test2); 
+  everything.addSensor(&test3); 
+  everything.addSensor(&test4); 
+  everything.addSensor(&test5); 
+  everything.addSensor(&test6);
+  everything.addSensor(&test7); 
+  everything.addSensor(new st::InterruptSensor("Alert", 11, HIGH));
+  everything.addSensor(new st::InterruptSensor("GarbageDisposal", 12, HIGH));
+  everything.addSensor(new st::PollingSensor("dynamicPolling", 2000, 0));
+  
+  everything.init();
+}
 
 void loop()
 {
-  for(unsigned int count=0; count<SENSOR_COUNT; ++count)
-  {
-     sensors[count]->update(); 
-  }
+  everything.run();
 }
