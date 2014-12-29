2b6b232dd2cd8fecf1eef0e5730cf76f8a6bc23f
Daniel Ogorchock <djogorchock@gmail.com>
2014-12-28 22:48:53 -0500

    updated test sketch

--- Arduino/ST_Test.ino
+++ Arduino/ST_Test.ino
@@ -12,11 +12,13 @@
 #include <PS_Water.h>
 #include <IS_Motion.h>
 #include <Sensor.h>
+#include <EX_Switch.h>
 
 #define PIN_ILLUMINANCE          A5
 #define PIN_TEMPERATUREHUMIDITY  5
 #define PIN_WATER                A4
 #define PIN_MOTION               4
+#define PIN_SWITCH               6
 
 long lastmillis=0;
 
@@ -25,6 +27,7 @@ st::PS_Illuminance sensor1("illuminance", 60000, 1000, PIN_ILLUMINANCE);
 st::PS_TemperatureHumidity sensor2("temphumid", 60000, 2000, PIN_TEMPERATUREHUMIDITY, st::PS_TemperatureHumidity::DHT22);
 st::PS_Water sensor3("water", 60000, 3000, PIN_WATER);
 st::IS_Motion sensor4("motion", PIN_MOTION, HIGH);
+st::EX_Switch executor1("switch", PIN_SWITCH, LOW);
 
 //test sketch
 void setup()
@@ -41,6 +44,9 @@ void setup()
   st::Everything::addSensor(&sensor2);
   st::Everything::addSensor(&sensor3);
   st::Everything::addSensor(&sensor4); 
+  
+  //Add each executor to the "Everything Class"
+  st::Everything::addExecutor(&executor1);
 
   st::Everything::initDevices();
 }
