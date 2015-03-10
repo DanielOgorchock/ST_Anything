d2ec1d6ee85f234f4380138a1376858a57db87aa
ogiewon <danogorchock@yahoo.com>
2015-03-10 14:45:54 -0400

    Added Furnace Alarm example for ST User
    Note - this example is extremely stripped down, requiring the
    ST_Anything Arduino Library to only include base classes + IS_Contact
    class.  This allows the sketch to be compiled without the DHT and
    RCSwitch libraries.

---
+++ Groovy/ST_Anything_FurnaceAlarm/ST_Anything_FurnaceAlarm.device.groovy
@@ -0,0 +1,65 @@
+/**
+ *  ST_Anything_FurnaceAlarm.groovy
+ *
+ *  Copyright 2015 Dan G Ogorchock 
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2015-03-10  Dan            Original Creation
+ *
+ */
+ 
+metadata {
+	definition (name: "ST_AnyThing_FurnaceAlarm", namespace: "ogiewon", author: "Daniel Ogorchock") {
+		capability "Contact Sensor"
+	}
+
+    simulator { 
+    }
+
+    // Preferences
+	preferences {
+	}
+
+	// Tile Definitions
+	tiles {
+
+		standardTile("contact", "device.contact", width: 1, height: 1) {
+			state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e")
+			state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821")
+		}
+
+        main(["contact"])
+        details(["contact"])
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+    def msg = zigbee.parse(description)?.text
+    log.debug "Parse got '${msg}'"
+
+    def parts = msg.split(" ")
+    def name  = parts.length>0?parts[0].trim():null
+    def value = parts.length>1?parts[1].trim():null
+
+    name = value != "ping" ? name : null
+
+    def result = createEvent(name: name, value: value)
+
+    log.debug result
+
+    return result
+}
