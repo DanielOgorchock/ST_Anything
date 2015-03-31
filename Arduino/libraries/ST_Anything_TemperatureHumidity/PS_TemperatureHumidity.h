70a6a3b6de906b5d8371cbf56a87e3105507cc1b
ogiewon <danogorchock@yahoo.com>
2015-03-30 21:14:47 -0400

    Memory Optimization for DHT Sensors

--- Arduino/libraries/ST_Anything_TemperatureHumidity/PS_TemperatureHumidity.h
+++ Arduino/libraries/ST_Anything_TemperatureHumidity/PS_TemperatureHumidity.h
@@ -31,6 +31,7 @@
 //    ----        ---            ----
 //    2015-01-03  Dan & Daniel   Original Creation
 //	  2015-01-17  Dan Ogorchock	 Added optional temperature and humidity device names in constructor to allow multiple Temp/Humidity sensors
+//    2015-03-29  Dan Ogorchock	 Optimized use of the DHT library (made it static) to reduce SRAM memory usage at runtime.
 //
 //
 //******************************************************************************************
@@ -49,7 +50,7 @@ namespace st
 			byte m_nDigitalInputPin;		//digital pin connected to the DHT sensor
 			int m_nTemperatureSensorValue;	//current Temperature value
 			int m_nHumiditySensorValue;		//current Humidity Value
-			dht DHT;						//DHT library object
+			static dht DHT;					//DHT library object
 			byte m_bDHTSensorType;			//DHT Sensor Type
 			String m_strTemperature;			//name of temparature sensor to use when transferring data to ST Cloud
 			String m_strHumidity;			//name of temparature sensor to use when transferring data to ST Cloud		
