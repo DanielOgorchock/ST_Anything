39361b8e2a1984cbdbb4c130f7ccd657af715fe3
Allan <vseven@yahoo.com>
2017-08-24 13:05:04 -0400

    Update child-contact-sensor.groovy
    Added a generateEvent routine to accept the data being passed by the parent device so the device knows when it is being updated and should allow future customization.  Also added a lastUpdated attribute that displays under the main tile.

--- devicetypes/ogiewon/child-contact-sensor.src/child-contact-sensor.groovy
+++ devicetypes/ogiewon/child-contact-sensor.src/child-contact-sensor.groovy
@@ -14,16 +14,23 @@
  *
  *  Change History:
  *
- *    Date        Who            What
- *    ----        ---            ----
- *    2017-04-10  Dan Ogorchock  Original Creation
+ *    	Date		Who		What
+ *    	----		---		----
+ *    	2017-04-10	Dan Ogorchock  	Original Creation
+ *	2017-08-23  	Allan (vseven) 	Added a generateEvent routine that gets info from the parent device.  This routine runs each time the value is updated which can lead to other modifications of the device.
+ *	2017-08-24  	Allan (vseven) 	Added a lastUpdated attribute that will display on the multitile.
  *
  * 
  */
 metadata {
 	definition (name: "Child Contact Sensor", namespace: "ogiewon", author: "Dan Ogorchock") {
 		capability "Contact Sensor"
-		capability "Sensor"
+		capability "Sensor"        
+        	attribute "lastUpdated", "String"
+	}
+
+	simulator {
+
 	}
 
 	tiles(scale: 2) {
@@ -32,7 +39,21 @@ metadata {
 				attributeState "open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#e86d13"
 				attributeState "closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#00a0dc"
             }
+			tileAttribute("device.lastUpdated", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'    Last updated ${currentValue}',icon: "st.Health & Wellness.health9")
+            }
         }
+	main "contact"
+        details(["contact", "lastUpdated"])
 	}
+}
 
+def generateEvent(String name, String value) {
+	// log.debug("Passed values to routine generateEvent in device named $device: Name - $name  -  Value - $value")
+	// Update device
+	sendEvent(name: name,value: value)
+    	// Update lastUpdated date and time
+    	def nowDay = new Date().format("MMM dd", location.timeZone)
+    	def nowTime = new Date().format("h:mm a", location.timeZone)
+    	sendEvent(name: "lastUpdated", value: nowDay + " at " + nowTime)
 }
