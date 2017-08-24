35c318644fe25027c79f054f299d147fb89c5a6f
Allan <vseven@yahoo.com>
2017-08-24 13:41:05 -0400

    Update child-voltage-sensor.groovy
    Added a generateEvent routine to accept the data being passed by the parent device so the device knows when it is being updated and should allow future customization.  Also added a lastUpdated attribute that displays under the main tile.

--- devicetypes/ogiewon/child-voltage-sensor.src/child-voltage-sensor.groovy
+++ devicetypes/ogiewon/child-voltage-sensor.src/child-voltage-sensor.groovy
@@ -17,6 +17,8 @@
  *    Date        Who            What
  *    ----        ---            ----
  *    2017-04-19  Dan Ogorchock  Original Creation
+ *    2017-08-23  Allan (vseven) Added a generateEvent routine that gets info from the parent device.  This routine runs each time the value is updated which can lead to other modifications of the device.
+ *    2017-08-24  Allan (vseven) Added a lastUpdated attribute that will display on the multitile.
  *
  * 
  */
@@ -24,6 +26,12 @@ metadata {
 	definition (name: "Child Voltage Sensor", namespace: "ogiewon", author: "Daniel Ogorchock") {
 		capability "Voltage Measurement"
 		capability "Sensor"
+       
+		attribute "lastUpdated", "String"
+	}
+
+	simulator {
+
 	}
         
 	tiles(scale: 2) {
@@ -31,6 +39,21 @@ metadata {
 			tileAttribute("device.voltage", key: "PRIMARY_CONTROL") {
 				attributeState("voltage", label: '${currentValue} ${unit}', unit: "mV", defaultState: true)
 			}
+			tileAttribute("device.lastUpdated", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'    Last updated ${currentValue}',icon: "st.Health & Wellness.health9")
+             		}
 		}
+		main(["temperature"])
+        	details(["temperature", "lastUpdate"])
 	}
+}
+
+def generateEvent(String name, String value) {
+	//log.debug("Passed values to routine generateEvent in device named $device: Name - $name  -  Value - $value")
+	// Update device
+	sendEvent(name: name,value: value)
+    	// Update lastUpdated date and time
+    	def nowDay = new Date().format("MMM dd", location.timeZone)
+    	def nowTime = new Date().format("h:mm a", location.timeZone)
+    	sendEvent(name: "lastUpdated", value: nowDay + " at " + nowTime)
 }
