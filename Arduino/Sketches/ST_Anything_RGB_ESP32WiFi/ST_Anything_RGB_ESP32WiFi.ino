1152cc771d285b8d67c94239d22a05d0e6bb0bd4
Allan <vseven@yahoo.com>
2017-10-08 22:39:22 -0400

    Create ST_Anything_RGB_ESP32WiFi.ino

---
+++ Arduino/Sketches/ST_Anything_RGB_ESP32WiFi/ST_Anything_RGB_ESP32WiFi.ino
@@ -0,0 +1,207 @@
+//******************************************************************************************
+//  File: ST_Anything_RGB_ESP32WiFi.ino
+//  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son)
+//
+//  Summary:  This Arduino Sketch, along with the ST_Anything library and the revised SmartThings 
+//            library, demonstrates the ability of one ESP32 to implement 
+//            a multi input/output custom device for integration into SmartThings.
+//            The ST_Anything library takes care of all of the work to schedule device updates
+//            as well as all communications with the ESP32's WiFi.
+//
+//            ST_Anything_RGB implements the following ST Capabilities as a demo of what is possible with a single ESP32
+//              - 2 x RGB LED (used to set the color of a RGB LED - Both individual RGB LED's and RGB LED Strips)
+//
+//    
+//  Change History:
+//
+//    Date        Who            What
+//    ----        ---            ----
+//    2017-08-14  Dan Ogorchock  Original Creation - Adapted from ESP8266 to work with ESP32 board
+//    2017-10-06  Allan (vseven) Modified for RGB examples
+//
+//   Special thanks to Joshua Spain for his contributions in porting ST_Anything to the ESP32!
+//
+//******************************************************************************************
+//******************************************************************************************
+// SmartThings Library for ESP32WiFi
+//******************************************************************************************
+#include <SmartThingsESP32WiFi.h>
+
+//******************************************************************************************
+// ST_Anything Library 
+//******************************************************************************************
+#include <Constants.h>       //Constants.h is designed to be modified by the end user to adjust behavior of the ST_Anything library
+#include <Device.h>          //Generic Device Class, inherited by Sensor and Executor classes
+#include <Sensor.h>          //Generic Sensor Class, typically provides data to ST Cloud (e.g. Temperature, Motion, etc...)
+#include <Executor.h>        //Generic Executor Class, typically receives data from ST Cloud (e.g. Switch)
+#include <InterruptSensor.h> //Generic Interrupt "Sensor" Class, waits for change of state on digital input 
+#include <PollingSensor.h>   //Generic Polling "Sensor" Class, polls Arduino pins periodically
+#include <Everything.h>      //Master Brain of ST_Anything library that ties everything together and performs ST Shield communications
+
+#include <EX_RGB_Dim.h>      //Implements an Executor (EX) for a RGB LED or strip with PWM using 3 digital output pins
+
+//****************************************************************************************************************************
+//NodeMCU-32s ESP32 Pin Definitions (just for reference from ..hardware\espressif\esp32\variants\nodemcu-32s\pins_arduino.h)
+//****************************************************************************************************************************
+//#define LED_BUILTIN 2
+//#define BUILTIN_LED 2
+//
+//#define A0 = 36;
+//#define A3 = 39;
+//#define A4 = 32;
+//#define A5 = 33;
+//#define A6 = 34;
+//#define A7 = 35;
+//#define A10 = 4;
+//#define A11 = 0;
+//#define A12 = 2;
+//#define A13 = 15;
+//#define A14 = 13;
+//#define A15 = 12;
+//#define A16 = 14;
+//#define A17 = 27;
+//#define A18 = 25;
+//#define A19 = 26;
+
+//******************************************************************************************
+//Define which Arduino Pins will be used for each device
+//******************************************************************************************
+//"RESERVED" pins for ESP32 - best to avoid
+#define PIN_0_RESERVED             0  //reserved ESP32 boot/program upload
+#define PIN_1_RESERVED             1  //reserved ESP32 for TX0
+#define PIN_3_RESERVED             3  //reserved ESP32 for RX0
+#define PIN_6_RESERVED             6  //reserved ESP32 for flash
+#define PIN_7_RESERVED             7  //reserved ESP32 for flash
+#define PIN_8_RESERVED             8  //reserved ESP32 for flash
+#define PIN_9_RESERVED             9  //reserved ESP32 for flash
+#define PIN_10_RESERVED           10  //reserved ESP32 for flash
+#define PIN_11_RESERVED           11  //reserved ESP32 for flash
+
+//Analog Pins
+
+//Digital Pins
+#define PIN_RGB1_Red           A0  //(GPIO 36) SmartThings Capability "Color Control"
+#define PIN_RGB1_Green         A3  //(GPIO 39) SmartThings Capability "Color Control"
+#define PIN_RGB1_Blue          A6  //(GPIO 34) SmartThings Capability "Color Control"
+#define PIN_RGB2_Red           A4  //(GPIO 32) SmartThings Capability "Color Control"
+#define PIN_RGB2_Green         A5  //(GPIO 33) SmartThings Capability "Color Control"
+#define PIN_RGB2_Blue          A18 //(GPIO 25) SmartThings Capability "Color Control"
+
+//******************************************************************************************
+//ESP832 WiFi Information
+//******************************************************************************************
+String str_ssid     = "yourSSIDhere";                           //  <---You must edit this line!
+String str_password = "yourWiFiPasswordhere";                   //  <---You must edit this line!
+IPAddress ip(192, 168, 1, 227);       //Device IP Address       //  <---You must edit this line!
+IPAddress gateway(192, 168, 1, 1);    //Router gateway          //  <---You must edit this line!
+IPAddress subnet(255, 255, 255, 0);   //LAN subnet mask         //  <---You must edit this line!
+IPAddress dnsserver(192, 168, 1, 1);  //DNS server              //  <---You must edit this line!
+const unsigned int serverPort = 8090; // port to run the http server on
+
+// Smartthings Hub Information
+IPAddress hubIp(192, 168, 1, 149);    // smartthings hub ip     //  <---You must edit this line!
+const unsigned int hubPort = 39500;   // smartthings hub port
+
+//******************************************************************************************
+//st::Everything::callOnMsgSend() optional callback routine.  This is a sniffer to monitor 
+//    data being sent to ST.  This allows a user to act on data changes locally within the 
+//    Arduino sktech.
+//******************************************************************************************
+void callback(const String &msg)
+{
+//  Serial.print(F("ST_Anything Callback: Sniffed data = "));
+//  Serial.println(msg);
+  
+  //TODO:  Add local logic here to take action when a device's value/state is changed
+  
+  //Masquerade as the ThingShield to send data to the Arduino, as if from the ST Cloud (uncomment and edit following line)
+  //st::receiveSmartString("Put your command here!");  //use same strings that the Device Handler would send
+}
+
+//******************************************************************************************
+//Arduino Setup() routine
+//******************************************************************************************
+void setup()
+{
+  //******************************************************************************************
+  //Declare each Device that is attached to the Arduino
+  //  Notes: - For each device, there is typically a corresponding "tile" defined in your 
+  //           SmartThings Device Hanlder Groovy code, except when using new COMPOSITE Device Handler
+  //         - For details on each device's constructor arguments below, please refer to the 
+  //           corresponding header (.h) and program (.cpp) files.
+  //         - The name assigned to each device (1st argument below) must match the Groovy
+  //           Device Handler names.  (Note: "temphumid" below is the exception to this rule
+  //           as the DHT sensors produce both "temperature" and "humidity".  Data from that
+  //           particular sensor is sent to the ST Hub in two separate updates, one for 
+  //           "temperature" and one for "humidity")
+  //         - The new Composite Device Handler is comprised of a Parent DH and various Child
+  //           DH's.  The names used below MUST not be changed for the Automatic Creation of
+  //           child devices to work properly.  Simply increment the number by +1 for each duplicate
+  //           device (e.g. contact1, contact2, contact3, etc...)  You can rename the Child Devices
+  //           to match your specific use case in the ST Phone Application.
+  //******************************************************************************************
+  //Polling Sensors
+  
+  //Interrupt Sensors 
+  
+  //Special sensors/executors (uses portions of both polling and executor classes)
+  
+  //Executors
+  static st::EX_RGB_Dim         executor1(F("RGBLED1"), PIN_RGB1_Red, PIN_RGB1_Green, PIN_RGB1_Blue, true, 0, 1, 2);  // channels (0,1,2) must be unique per ESP32
+  static st::EX_RGB_Dim         executor2(F("RGBLED2"), PIN_RGB2_Red, PIN_RGB2_Green, PIN_RGB2_Blue, true, 3, 4, 5);  // therefor we continue the sequence (3,4,5) here
+  
+  //*****************************************************************************
+  //  Configure debug print output from each main class 
+  //  -Note: Set these to "false" if using Hardware Serial on pins 0 & 1
+  //         to prevent communication conflicts with the ST Shield communications
+  //*****************************************************************************
+  st::Everything::debug=true;
+  st::Executor::debug=true;
+  st::Device::debug=true;
+  st::PollingSensor::debug=true;
+  st::InterruptSensor::debug=true;
+
+  //*****************************************************************************
+  //Initialize the "Everything" Class
+  //*****************************************************************************
+
+  //Initialize the optional local callback routine (safe to comment out if not desired)
+  st::Everything::callOnMsgSend = callback;
+  
+  //Create the SmartThings ESP32WiFi Communications Object
+    //STATIC IP Assignment - Recommended
+    st::Everything::SmartThing = new st::SmartThingsESP32WiFi(str_ssid, str_password, ip, gateway, subnet, dnsserver, serverPort, hubIp, hubPort, st::receiveSmartString);
+ 
+    //DHCP IP Assigment - Must set your router's DHCP server to provice a static IP address for this device's MAC address
+    //st::Everything::SmartThing = new st::SmartThingsESP32WiFi(str_ssid, str_password, serverPort, hubIp, hubPort, st::receiveSmartString);
+
+  //Run the Everything class' init() routine which establishes WiFi communications with SmartThings Hub
+  st::Everything::init();
+  
+  //*****************************************************************************
+  //Add each sensor to the "Everything" Class
+  //*****************************************************************************
+      
+  //*****************************************************************************
+  //Add each executor to the "Everything" Class
+  //*****************************************************************************
+  st::Everything::addExecutor(&executor1);
+  st::Everything::addExecutor(&executor2);
+      
+  //*****************************************************************************
+  //Initialize each of the devices which were added to the Everything Class
+  //*****************************************************************************
+  st::Everything::initDevices();
+  
+}
+
+//******************************************************************************************
+//Arduino Loop() routine
+//******************************************************************************************
+void loop()
+{
+  //*****************************************************************************
+  //Execute the Everything run method which takes care of "Everything"
+  //*****************************************************************************
+  st::Everything::run();
+}
