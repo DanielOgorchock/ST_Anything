94e098f47850866f5a7b73824e390a9b9be2f387
ogiewon <danogorchock@yahoo.com>
2018-02-09 20:53:25 -0500

    Added Hubitat Support
    Added Hubitat Support - Hubitat specific Bridge Drivers and Service
    Manager App

---
+++ HubDuino/Apps/hubduino-service-manager.groovy
@@ -0,0 +1,307 @@
+/**
+ *  HubDuino Service Manager
+ *
+ *  Copyright 2018 Daniel Ogorchock
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2018-02-08  Dan Ogorchock  Original Creation
+ *
+ */
+ 
+definition(
+    name: "HubDuino Service Manager",
+    namespace: "ogiewon",
+    author: "Daniel Ogorchock",
+    description: "Connects HubDuino Bridge to virtual Child Devices",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+	section("Select the HubDuino Bridge Device") {
+		input "HubDuino", "capability.sensor"
+    }    
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	subscribe()
+    HubDuino.refresh()
+}
+
+def uninstalled() {
+    childDevices.each { deleteChildDevice(it.deviceNetworkId) }
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	subscribe()
+    HubDuino.refresh()
+}
+
+def subscribe() {
+    subscribe(HubDuino, "all", childHandler)
+}
+
+def childHandler(evt) {
+	def parts = evt.value.split(":")
+	def name  = parts.length>0?parts[0].trim():null
+	def value = parts.length>0?parts[1].trim():null
+    log.debug "HubDuino Event(name: ${name}, value: ${value}, deviceID: ${evt.deviceId})"
+    
+	def nameparts = name.split("\\d+", 2)
+	def namebase = nameparts.length>0?nameparts[0].trim():null
+    def namenum = name.substring(namebase.length()).trim()   
+    
+    def isChild = containsDigit(name)
+	def childDevice = null
+
+		try {
+
+            childDevices.each {
+				try{
+            		//log.debug "Looking for child with app.id = ${HubDuino.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+                	if (it.deviceNetworkId == "${HubDuino.deviceNetworkId}-${name}") {
+                	childDevice = it
+                    //log.debug "Found a match!!!"
+                	}
+            	}
+            	catch (e) {
+            		log.debug e
+            	}
+        	}
+            
+            //If a child should exist, but doesn't yet, automatically add it!            
+        	if (isChild && childDevice == null) {
+        		log.debug "isChild = true, but no child found - Auto Add it!"
+            	log.debug "    Need a ${namebase} with id = ${namenum}"
+            
+            	createChildDevice(namebase, namenum)
+            	//find child again, since it should now exist!
+            	childDevices.each {
+					try{
+            			//log.debug "Looking for child with deviceNetworkID = ${HubDuino.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+                		if (it.deviceNetworkId == "${HubDuino.deviceNetworkId}-${name}") {
+                			childDevice = it
+                    		//log.debug "Found a match!!!"
+                		}
+            		}
+            		catch (e) {
+            			log.debug e
+            		}
+        		}
+        	}
+            
+            if (childDevice != null) {
+                //log.debug "childHandler() found child device ${childDevice.deviceNetworkId}"
+                
+                childDevice.generateEvent(namebase, value)
+				log.debug "${childDevice.deviceNetworkId} - name: ${namebase}, value: ${value}"
+
+            }
+            else  //must not be a child, perform normal update
+            {
+                //log.debug "Error in childHandler() - not a child device!"
+            }
+		}
+        catch (e) {
+        	log.error "Error in childHandler() routine, error = ${e}"
+        }   
+}
+
+def childOn(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} on")
+}
+
+def childOff(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childOff($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} off")
+}
+
+def childAlarmOn(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} both")
+}
+
+def childAlarmSiren(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} siren")
+}
+
+def childAlarmStrobe(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} strobe")
+}
+
+def childAlarmBoth(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} both")
+}
+
+def childAlarmOff(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmOff($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} off")
+}
+
+def childAlarmTest(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childAlarmTest($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} both")
+	runIn(3, childAlarmTestOff, [data: [devicenetworkid: dni]])
+}
+
+def childAlarmTestOff(data) {
+	childAlarmOff(data.devicenetworkid)
+}
+
+def childDoorOpen(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childDoorOpen($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} on")
+}
+
+def childDoorClose(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childDoorClose($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} on")
+}
+
+def childSetLevel(String dni, value) {
+    def name = dni.split("-")[-1]
+    log.debug "childSetLevel($dni), name = ${name}, level = ${value}"
+    HubDuino.sendEthernet("${name} ${value}")
+}
+
+def childSetColorRGB(String dni, value) {
+    def name = dni.split("-")[-1]
+    log.debug "childSetColorRGB($dni), name = ${name}, colorRGB = ${value}"
+    HubDuino.sendEthernet("${name} ${value}")
+}
+
+def childSetColorRGBW(String dni, value) {
+    def name = dni.split("-")[-1]
+    log.debug "childSetColorRGBW($dni), name = ${name}, colorRGBW = ${value}"
+    HubDuino.sendEthernet("${name} ${value}")
+}
+
+def childRelayOn(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childRelayOn($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} on")
+}
+
+def childRelayOff(String dni) {
+    def name = dni.split("-")[-1]
+    log.debug "childRelayOff($dni), name = ${name}"
+    HubDuino.sendEthernet("${name} off")
+}
+
+private void createChildDevice(String deviceName, String deviceNumber) {
+    
+		log.trace "createChildDevice:  Creating Child Device '${HubDuino.deviceNetworkId}-${deviceName}${deviceNumber}'"
+        
+		try {
+        	def deviceHandlerName = ""
+        	switch (deviceName) {
+         		case "contact": 
+                		deviceHandlerName = "Child Contact Sensor" 
+                	break
+         		case "switch": 
+                		deviceHandlerName = "Child Switch" 
+                	break
+         		case "dimmerSwitch": 
+                		deviceHandlerName = "Child Dimmer Switch" 
+                	break
+         		case "rgbSwitch": 
+                		deviceHandlerName = "Child RGB Switch" 
+                	break
+         		case "generic": 
+                		deviceHandlerName = "Child Generic Sensor" 
+                	break
+         		case "rgbwSwitch": 
+                		deviceHandlerName = "Child RGBW Switch" 
+                	break
+         		case "relaySwitch": 
+                		deviceHandlerName = "Child Relay Switch" 
+                	break
+         		case "temperature": 
+                		deviceHandlerName = "Child Temperature Sensor" 
+                	break
+         		case "humidity": 
+                		deviceHandlerName = "Child Humidity Sensor" 
+                	break
+         		case "motion": 
+                		deviceHandlerName = "Child Motion Sensor" 
+                	break
+         		case "water": 
+                		deviceHandlerName = "Child Water Sensor" 
+                	break
+         		case "illuminance": 
+                		deviceHandlerName = "Child Illuminance Sensor" 
+                	break
+         		case "illuminancergb": 
+                		deviceHandlerName = "Child IlluminanceRGB Sensor" 
+                	break
+         		case "voltage": 
+                		deviceHandlerName = "Child Voltage Sensor" 
+                	break
+         		case "smoke": 
+                		deviceHandlerName = "Child Smoke Detector" 
+                	break    
+         		case "carbonMonoxide": 
+                		deviceHandlerName = "Child Carbon Monoxide Detector" 
+                	break    
+         		case "alarm": 
+                		deviceHandlerName = "Child Alarm" 
+                	break    
+         		case "doorControl": 
+                		deviceHandlerName = "Child Door Control" 
+                	break
+			default: 
+                		log.error "No Child Device Handler case for ${deviceName}"
+      		}
+            if (deviceHandlerName != "") {
+    			def child = addChildDevice("ogiewon", deviceHandlerName, "${HubDuino.deviceNetworkId}-${deviceName}${deviceNumber}", null, [name: "${app.id}-${deviceName}${deviceNumber}", label: "${app.label} ${deviceName}${deviceNumber}", completedSetup: true])
+        	}   
+    	} catch (e) {
+        	log.error "Child device creation failed with error = ${e}"
+        	log.debug "Child device creation failed. Please make sure that the '${deviceHandlerName}' is installed and published."
+	    	
+    	}
+}
+
+private boolean containsDigit(String s) {
+    boolean containsDigit = false;
+
+    if (s != null && !s.isEmpty()) {
+//		log.debug "containsDigit .matches = ${s.matches(".*\\d+.*")}"
+		containsDigit = s.matches(".*\\d+.*")
+    }
+    return containsDigit
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
---
+++ HubDuino/Drivers/hubduino-ethernet-bridge.groovy
@@ -0,0 +1,181 @@
+﻿/**
+ *  HubDuino Ethernet Bridge - Driver
+ *
+ *  Copyright 2018 Dan G Ogorchock 
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2018-02-07  Dan Ogorchock  Original Creation
+ *
+ */
+metadata {
+	definition (name: "HubDuino Ethernet Bridge", namespace: "ogiewon", author: "Dan Ogorchock") {
+//        capability "Bridge"
+        capability "Sensor"
+        capability "Configuration"
+        capability "Refresh"
+        capability "Pushable Button"
+        capability "Holdable Button"
+        capability "DoubleTapableButton"
+        capability "Signal Strength" }
+
+    // Preferences
+	preferences {
+		input "ip", "text", title: "Arduino IP Address", description: "IP Address in form 192.168.1.226", required: true, displayDuringSetup: true
+		input "port", "text", title: "Arduino Port", description: "port in form of 8090", required: true, displayDuringSetup: true
+		input "mac", "text", title: "Arduino MAC Addr", description: "MAC Address in form of 02A1B2C3D4E5", required: true, displayDuringSetup: true
+		input "numButtons", "number", title: "Number of Buttons", description: "Number of Buttons, 0 to n", required: true, displayDuringSetup: true
+	}
+
+}
+
+// parse events into attributes
+def parse(String description) {
+	//log.debug "Parsing '${description}'"
+	def msg = parseLanMessage(description)
+	def headerString = msg.header
+
+	if (!headerString) {
+		//log.debug "headerString was null for some reason :("
+    }
+    else {
+		def bodyString = msg.body
+
+		if (bodyString) {
+        	//log.debug "bodyString: $bodyString"
+    		def parts = bodyString.split(" ")
+    		def name  = parts.length>0?parts[0].trim():null
+    		def value = parts.length>0?parts[1].trim():null
+            
+            def nameparts = name.split("\\d+", 2)
+			def namebase = nameparts.length>0?nameparts[0].trim():null
+        	def namenum = name.substring(namebase.length()).trim()
+            
+            def results = []
+            
+            if (name.startsWith("button")) {
+				//log.debug "In parse:  name = ${name}, value = ${value}, btnNum = " + namenum
+        		results << createEvent(name: value, value: namenum)             
+        	}
+			else {
+			    results << createEvent(name: name, value: value)
+                if (!name.startsWith("rssi")) { 
+                    results << createEvent(name: "all", value: "${name}:${value}") 
+                }
+            }
+            log.debug "results = " + results
+			return results
+		}
+        else {
+        	//log.debug "bodyString was null for some reason :("
+        }
+    }
+}
+
+private getHostAddress() {
+    def ip = settings.ip
+    def port = settings.port
+
+	log.debug "Using ip: ${ip} and port: ${port} for device: ${device.id}"
+    return ip + ":" + port
+}
+
+def sendEthernet(message) {
+	log.debug "Executing 'sendEthernet' ${message}"
+	if (settings.ip != null && settings.port != null) {
+    	new hubitat.device.HubAction(
+    		method: "POST",
+    		path: "/${message}?",
+    		headers: [ HOST: "${getHostAddress()}" ]
+		)
+    }
+    else {
+    	log.debug "HubDuino Ethernet Bridge Device: Please verify IP address and Port are configured."    
+    }
+}
+
+// handle commands
+def push(button) {
+    log.debug "push(${button})"
+    sendEvent(name: "pushed", value: button)
+}
+
+def hold(button) {
+    log.debug "hold(${button})"    
+    sendEvent(name: "hold", value: button)
+}
+
+def doubleTap(button) {
+    log.debug "doubleTap(${button})"    
+    sendEvent(name: "doubleTapped", value: button)
+}
+   
+
+
+def configure() {
+	log.debug "Executing 'configure()'"
+    updateDeviceNetworkID()
+	sendEvent(name: "numberOfButtons", value: numButtons)
+}
+
+def refresh() {
+	log.debug "Executing 'refresh()'"
+	sendEthernet("refresh")
+	//sendEvent(name: "numberOfButtons", value: numButtons)
+}
+
+
+def installed() {
+	log.debug "Executing 'installed()'"
+    if ( device.deviceNetworkId =~ /^[A-Z0-9]{12}$/)
+    {
+        if (numButtons) { sendEvent(name: "numberOfButtons", value: numButtons) }
+    }
+    else
+    {
+        log.error "HubDuino Ethernet Bridge Device has not been fully configured."
+    }
+}
+
+def initialize() {
+	log.debug "Executing 'initialize()'"
+    sendEvent(name: "numberOfButtons", value: numButtons)
+}
+
+def updated() {
+	if (!state.updatedLastRanAt || now() >= state.updatedLastRanAt + 5000) {
+		state.updatedLastRanAt = now()
+		log.debug "Executing 'updated()'"
+    	updateDeviceNetworkID()
+		sendEvent(name: "numberOfButtons", value: numButtons)
+        log.debug "Hub IP Address = ${device.hub.getDataValue("localIP")}"
+        log.debug "Hub Port = ${device.hub.getDataValue("localSrvPortTCP")}"
+	}
+	else {
+		log.trace "updated(): Ran within last 5 seconds so aborting."
+	}
+}
+
+
+def updateDeviceNetworkID() {
+	log.debug "Executing 'updateDeviceNetworkID'"
+    if(device.deviceNetworkId!=mac) {
+    	log.debug "setting deviceNetworkID = ${mac}"
+        device.setDeviceNetworkId("${mac}")
+	}
+    //Need deviceNetworkID updated BEFORE we can create Child Devices
+	//Have the Arduino send an updated value for every device attached.  This will auto-created child devices!
+	refresh()
+}
+
