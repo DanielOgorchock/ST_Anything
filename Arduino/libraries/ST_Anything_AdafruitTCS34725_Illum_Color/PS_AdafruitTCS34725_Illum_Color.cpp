4d3c6aaaac51fd47b69101596192b44324d5f20d
ogiewon <danogorchock@yahoo.com>
2018-07-01 19:55:14 -0400

    Updated TCS34725 device

--- Arduino/libraries/ST_Anything_AdafruitTCS34725_Illum_Color/PS_AdafruitTCS34725_Illum_Color.cpp
+++ Arduino/libraries/ST_Anything_AdafruitTCS34725_Illum_Color/PS_AdafruitTCS34725_Illum_Color.cpp
@@ -3,21 +3,32 @@
 //  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son)
 //
 //  Summary:  PS_AdafruitTCS34725_Illum_Color is a class which implements the SmartThings "Illuminance Measurement" device capability.
-//			  It inherits from the st::PollingSensor class.  The current version uses an analog input to measure the 
-//			  value of a simple photo resistor.
-//
-//			  The last four arguments of the constructor are used as arguments to an Arduino map() function which 
-//			  is used to scale the analog input readings (0 to 1024) to Lux before sending to SmartThings.  The
-//			  defaults for this sensor are based on the device used during testing.  
-//
+//			  It inherits from the st::PollingSensor class.  It uses I2C communication to measure the RGB Illuminace from a TCS34725 sensor.
+//			  
 //			  Create an instance of this class in your sketch's global variable section
-//			  For Example:  st::PS_AdafruitTCS34725_Illum_Color sensor1("illuminance1", 120, 0, PIN_ILLUMINANCE, 0, 1023, 0, 1000);
+//			  For Example:  st::PS_AdafruitTCS34725_Illum_Color sensor1(F("illuminancergb1"), 60, 0);
 //
 //			  st::PS_AdafruitTCS34725_Illum_Color() constructor requires the following arguments
 //				- String &name - REQUIRED - the name of the object - must match the Groovy ST_Anything DeviceType tile name
 //				- long interval - REQUIRED - the polling interval in seconds
 //				- long offset - REQUIRED - the polling interval offset in seconds - used to prevent all polling sensors from executing at the same time
+//				- tcs34725IntegrationTime_t integrationTime - OPTIONAL - defaults to TCS34725_INTEGRATIONTIME_154MS
+//				- tcs34725Gain_t gain - OPTIONAL - defaults to TCS34725_GAIN_4X
 //
+//              Valid Integration Time Values:
+//				  TCS34725_INTEGRATIONTIME_2_4MS = 0xFF,   /**<  2.4ms - 1 cycle    - Max Count: 1024  */
+//				  TCS34725_INTEGRATIONTIME_24MS = 0xF6,    /**<  24ms  - 10 cycles  - Max Count: 10240 */
+//				  TCS34725_INTEGRATIONTIME_50MS = 0xEB,    /**<  50ms  - 20 cycles  - Max Count: 20480 */
+//				  TCS34725_INTEGRATIONTIME_101MS = 0xD5,   /**<  101ms - 42 cycles  - Max Count: 43008 */
+//				  TCS34725_INTEGRATIONTIME_154MS = 0xC0,   /**<  154ms - 64 cycles  - Max Count: 65535 */
+//				  TCS34725_INTEGRATIONTIME_700MS = 0x00    /**<  700ms - 256 cycles - Max Count: 65535 */
+//
+//				Valid Gain Values:
+//				  TCS34725_GAIN_1X = 0x00,    /**<  No gain  */
+//				  TCS34725_GAIN_4X = 0x01,    /**<  4x gain  */
+//				  TCS34725_GAIN_16X = 0x02,   /**<  16x gain */
+//				  TCS34725_GAIN_60X = 0x03    /**<  60x gain */
+
 //			  This class supports receiving configuration data from the SmartThings cloud via the ST App.  A user preference
 //			  can be configured in your phone's ST App, and then the "Configure" tile will send the data for all sensors to 
 //			  the ST Shield.  For PollingSensors, this data is handled in the beSMart() function.
@@ -29,9 +40,10 @@
 //    Date        Who            What
 //    ----        ---            ----
 //    2015-01-03  Dan & Daniel   Original Creation
-//    2018-08-30  Dan Ogorchock  Modified comment section above to comply with new Parent/Child Device Handler requirements
+//    2017-08-30  Dan Ogorchock  Modified comment section above to comply with new Parent/Child Device Handler requirements
 //    2017-09-07  Allan (vseven) Modified original PS_Illuminance library for use with the Adafruit TCS34725 sensor
 //    2017-12-29  Allan (vseven) Fixed bug with improper init() definition per Dans guidance
+//    2018-07-01  Dan Ogorchock  Cleaned up the design, added ability to adjust configuration, and fixed the comments section
 //
 //
 //******************************************************************************************
@@ -41,22 +53,15 @@
 #include "Constants.h"
 #include "Everything.h"
 
-#include "Adafruit_TCS34725.h"
-
-/* Initialise with default values (int time = 2.4ms, gain = 1x) */
-Adafruit_TCS34725 tcs = Adafruit_TCS34725();
-
-/* Initialise with specific int time and gain values */
-//Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_1X);
-
 namespace st
 {
 //private
 
 //public
 	//constructor - called in your sketch's global variable declaration section
-	PS_AdafruitTCS34725_Illum_Color::PS_AdafruitTCS34725_Illum_Color(const __FlashStringHelper *name, unsigned int interval, int offset):
-		PollingSensor(name, interval, offset),m_nSensorValue(0)
+	PS_AdafruitTCS34725_Illum_Color::PS_AdafruitTCS34725_Illum_Color(const __FlashStringHelper *name, unsigned int interval, int offset, tcs34725IntegrationTime_t integrationTime, tcs34725Gain_t gain) :
+		PollingSensor(name, interval, offset),
+		tcs(integrationTime, gain)
 	{
 
 	}
@@ -90,37 +95,39 @@ namespace st
 	}
 
 	void PS_AdafruitTCS34725_Illum_Color::init() {
-	  	Serial.println("Initiating the TCS34725 sensor...");
-  		if (tcs.begin()) {
+		firstTime = true;
+		Serial.println("Initializing the TCS34725 sensor...");
+		if (tcs.begin()) {
 			Serial.println("Found sensor.   tcs.begin = true");
-		} else {
-			Serial.println("No TCS34725 found ... check your connections");
 		}
+		else {
+			Serial.println("No TCS34725 found... check your connections");
+		}
+		getData();
 	}
 	
 	//function to get data from sensor and queue results for transfer to ST Cloud 
 	void PS_AdafruitTCS34725_Illum_Color::getData()
 	{
-		//Serial.println("getData routine called");
-		uint16_t lux, colorTemp, red, green, blue, clear;
+  		tcs.getRawData(&m_nred, &m_ngreen, &m_nblue, &m_nclear);
+		m_ncolorTemp = tcs.calculateColorTemperature(m_nred, m_ngreen, m_nblue);
+		m_nlux = tcs.calculateLux(m_nred, m_ngreen, m_nblue);
 
-  		tcs.getRawData(&red, &green, &blue, &clear);
-  		colorTemp = tcs.calculateColorTemperature(red, green, blue);
- 		lux = tcs.calculateLux(red, green, blue);
+		//Serial.print("Color Temp: "); Serial.print(m_ncolorTemp, DEC); Serial.print(" K - ");
+		//Serial.print("Lux: "); Serial.print(m_nlux, DEC); Serial.print(" - ");
+		//Serial.print("R: "); Serial.print(m_nred, DEC); Serial.print(" ");
+		//Serial.print("G: "); Serial.print(m_ngreen, DEC); Serial.print(" ");
+		//Serial.print("B: "); Serial.print(m_nblue, DEC); Serial.print(" ");
+		//Serial.print("C: "); Serial.print(m_nclear, DEC); Serial.print(" ");
+		//Serial.println(" ");
 
-		//Serial.println(lux,DEC);
-		//Serial.println(colorTemp,DEC);
-		//Serial.println(red,DEC);
-		//Serial.println(green,DEC);
-		//Serial.println(blue,DEC);
-		//Serial.println(clear,DEC);
+		String strSensorValue = String(m_nlux, DEC) + ':' + String(m_ncolorTemp, DEC) + ':' + String(m_nred, DEC) + ':' + String(m_ngreen, DEC) + ':' + String(m_nblue, DEC) + ':' + String(m_nclear, DEC);
 
-		String m_nSensorValue = String(lux, DEC) + ':' + String(colorTemp, DEC) + ':' + String(red, DEC) + ':' + String(green, DEC) + ':' + String(blue, DEC) + ':' + String(clear, DEC);
-
-		//Serial.println(getName());
-		//Serial.println(m_nSensorValue);		
-
-		Everything::sendSmartString(getName() + " " + String(m_nSensorValue));
+		//Sensor data is invalid for first measurement, therefore do not send it to SmartThings/Hubitat
+		if (!firstTime) {
+			Everything::sendSmartString(getName() + " " + strSensorValue);
+		}
+		firstTime = false;
 	}
 	
 }
--- Arduino/libraries/ST_Anything_AdafruitTCS34725_Illum_Color/PS_AdafruitTCS34725_Illum_Color.h
+++ Arduino/libraries/ST_Anything_AdafruitTCS34725_Illum_Color/PS_AdafruitTCS34725_Illum_Color.h
@@ -3,21 +3,32 @@
 //  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son)
 //
 //  Summary:  PS_AdafruitTCS34725_Illum_Color is a class which implements the SmartThings "Illuminance Measurement" device capability.
-//			  It inherits from the st::PollingSensor class.  The current version uses an analog input to measure the 
-//			  value of a simple photo resistor.
-//
-//			  The last four arguments of the constructor are used as arguments to an Arduino map() function which 
-//			  is used to scale the analog input readings (0 to 1024) to Lux before sending to SmartThings.  The
-//			  defaults for this sensor are based on the device used during testing.  
-//
+//			  It inherits from the st::PollingSensor class.  It uses I2C communication to measure the RGB Illuminace from a TCS34725 sensor.
+//			  
 //			  Create an instance of this class in your sketch's global variable section
-//			  For Example:  st::PS_AdafruitTCS34725_Illum_Color sensor1("illuminance1", 120, 0, PIN_ILLUMINANCE, 0, 1023, 0, 1000);
+//			  For Example:  st::PS_AdafruitTCS34725_Illum_Color sensor1(F("illuminancergb1"), 60, 0);
 //
 //			  st::PS_AdafruitTCS34725_Illum_Color() constructor requires the following arguments
 //				- String &name - REQUIRED - the name of the object - must match the Groovy ST_Anything DeviceType tile name
 //				- long interval - REQUIRED - the polling interval in seconds
 //				- long offset - REQUIRED - the polling interval offset in seconds - used to prevent all polling sensors from executing at the same time
+//				- tcs34725IntegrationTime_t integrationTime - OPTIONAL - defaults to TCS34725_INTEGRATIONTIME_154MS
+//				- tcs34725Gain_t gain - OPTIONAL - defaults to TCS34725_GAIN_4X
 //
+//              Valid Integration Time Values:
+//				  TCS34725_INTEGRATIONTIME_2_4MS = 0xFF,   /**<  2.4ms - 1 cycle    - Max Count: 1024  */
+//				  TCS34725_INTEGRATIONTIME_24MS = 0xF6,    /**<  24ms  - 10 cycles  - Max Count: 10240 */
+//				  TCS34725_INTEGRATIONTIME_50MS = 0xEB,    /**<  50ms  - 20 cycles  - Max Count: 20480 */
+//				  TCS34725_INTEGRATIONTIME_101MS = 0xD5,   /**<  101ms - 42 cycles  - Max Count: 43008 */
+//				  TCS34725_INTEGRATIONTIME_154MS = 0xC0,   /**<  154ms - 64 cycles  - Max Count: 65535 */
+//				  TCS34725_INTEGRATIONTIME_700MS = 0x00    /**<  700ms - 256 cycles - Max Count: 65535 */
+//
+//				Valid Gain Values:
+//				  TCS34725_GAIN_1X = 0x00,    /**<  No gain  */
+//				  TCS34725_GAIN_4X = 0x01,    /**<  4x gain  */
+//				  TCS34725_GAIN_16X = 0x02,   /**<  16x gain */
+//				  TCS34725_GAIN_60X = 0x03    /**<  60x gain */
+
 //			  This class supports receiving configuration data from the SmartThings cloud via the ST App.  A user preference
 //			  can be configured in your phone's ST App, and then the "Configure" tile will send the data for all sensors to 
 //			  the ST Shield.  For PollingSensors, this data is handled in the beSMart() function.
@@ -32,6 +43,7 @@
 //    2017-08-30  Dan Ogorchock  Modified comment section above to comply with new Parent/Child Device Handler requirements
 //    2017-09-07  Allan (vseven) Modified original PS_Illuminance library for use with the Adafruit TCS34725 sensor
 //    2017-12-29  Allan (vseven) Fixed bug with improper init() definition per Dans guidance
+//    2018-07-01  Dan Ogorchock  Cleaned up the design, added ability to adjust configuration, and fixed the comments section
 //
 //
 //******************************************************************************************
@@ -40,17 +52,25 @@
 #define ST_PS_AdafruitTCS34725_Illum_Color_H
 
 #include "PollingSensor.h"
+#include "Adafruit_TCS34725.h"
 
 namespace st
 {
 	class PS_AdafruitTCS34725_Illum_Color: public PollingSensor
 	{
 		private:
-			char m_nSensorValue;  //converted to a string so all data can be passed in one call
-			
+			Adafruit_TCS34725 tcs;  //Adafruit TCS34725 object
+			uint16_t m_nlux;		//lux
+			uint16_t m_ncolorTemp;	//color temperature
+			uint16_t m_nred;		//red value
+			uint16_t m_ngreen;		//green value
+			uint16_t m_nblue;		//blue value
+			uint16_t m_nclear;		//clear value
+			bool firstTime;			//First reading is not good, so don't send it
+
 		public:
 			//constructor - called in your sketch's global variable declaration section
-			PS_AdafruitTCS34725_Illum_Color(const __FlashStringHelper *name, unsigned int interval, int offset);
+			PS_AdafruitTCS34725_Illum_Color(const __FlashStringHelper *name, unsigned int interval, int offset, tcs34725IntegrationTime_t integrationTime = TCS34725_INTEGRATIONTIME_154MS, tcs34725Gain_t gain = TCS34725_GAIN_4X);
 			
 			//destructor
 			virtual ~PS_AdafruitTCS34725_Illum_Color();
@@ -65,7 +85,6 @@ namespace st
 			virtual void getData();
 			
 			//gets
-			inline byte getSensorValue() const {return m_nSensorValue;}
 				
 			//sets
 			
