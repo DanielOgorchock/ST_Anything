e1a7961343635a7204e9b2ec23cde803fbee6815
ogiewon <danogorchock@yahoo.com>
2019-05-29 09:37:11 -0400

    Added LinkNode R8 Example Sketch

---
+++ Arduino/Sketches/ST_Anything_LinkNodeR8_ESP8266WiFi/ST_Anything_LinkNodeR8_ESP8266WiFi.ino
@@ -0,0 +1,189 @@
+//******************************************************************************************
+//  File: ST_Anything_LinkNodeR8_ESP8266WiFi.ino
+//  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son)
+//
+//  Summary:  This Arduino Sketch, along with the ST_Anything library and the revised SmartThings 
+//            library, demonstrates the ability of one LinkNodeR8 ESP8266 to 
+//            implement a multi input/output custom device for integration into SmartThings.
+//            The ST_Anything library takes care of all of the work to schedule device updates
+//            as well as all communications with the LinkNodeR8 ESP8266's WiFi.
+//
+//            ST_Anything_LinkNodeR8 implements the following Capabilities as a demo of what 
+//            is possible with a single LinkSprite LinkNode R8 board
+//              - 8 x Switch devices (used to turn on a digital output (e.g. LED, relay, etc...)
+//
+//  Board Programming details available at: http://www.linksprite.com/wiki/index.php?title=File:1-262-a.jpg
+//    
+//  Change History:
+//
+//    Date        Who            What
+//    ----        ---            ----
+//    2019-03-26  Dan Ogorchock  Original Creation
+//    2019-05-18  John Crusan    Modified LinkNode R4 Code to support LinkNode R8
+//
+//******************************************************************************************
+//******************************************************************************************
+// SmartThings Library for ESP8266WiFi
+//******************************************************************************************
+#include <SmartThingsESP8266WiFi.h>
+
+//******************************************************************************************
+// ST_Anything Library 
+//******************************************************************************************
+#include <Constants.h>       //Constants.h is designed to be modified by the end user to adjust behavior of the ST_Anything library
+#include <Device.h>          //Generic Device Class, inherited by Sensor and Executor classes
+#include <Sensor.h>          //Generic Sensor Class, typically provides data to ST Cloud (e.g. Temperature, Motion, etc...)
+#include <Executor.h>        //Generic Executor Class, typically receives data from ST Cloud (e.g. Switch)
+#include <InterruptSensor.h> //Generic Interrupt "Sensor" Class, waits for change of state on digital input 
+#include <PollingSensor.h>   //Generic Polling "Sensor" Class, polls Arduino pins periodically
+#include <Everything.h>      //Master Brain of ST_Anything library that ties everything together and performs ST Shield communications
+
+#include <EX_Switch.h>       //Implements an Executor (EX) via a digital output to a relay
+#include <S_TimedRelay.h>    //Implements a Sensor to control a digital output pin with timing capabilities
+
+
+//******************************************************************************************
+//Define which Arduino Pins will be used for each device
+//******************************************************************************************
+#define PIN_S1              14  //LinkNode R8 Board Marking D8
+#define PIN_S2              13  //LinkNode R8 Board Marking D7
+#define PIN_S3               5  //LinkNode R8 Board Marking D10
+#define PIN_S4              15  //LinkNode R8 Board Marking D11
+#define PIN_S5              12  //LinkNode R8 Board Marking D6
+#define PIN_S6              16  //LinkNode R8 Board Marking D9
+#define PIN_S7               4  //LinkNode R8 Board Marking D17
+#define PIN_S8              10  //LinkNode R8 Board Marking D12
+
+//******************************************************************************************
+//ESP8266 WiFi Information
+//******************************************************************************************
+String str_ssid     = "yourSSIDhere";                           //  <---You must edit this line!
+String str_password = "yourWiFiPasswordhere";                   //  <---You must edit this line!
+IPAddress ip(192, 168, 1, 127);       //Device IP Address       //  <---You must edit this line!
+IPAddress gateway(192, 168, 1, 1);    //Router gateway          //  <---You must edit this line!
+IPAddress subnet(255, 255, 255, 0);   //LAN subnet mask         //  <---You must edit this line!
+IPAddress dnsserver(192, 168, 1, 1);  //DNS server              //  <---You must edit this line!
+const unsigned int serverPort = 8090; // port to run the http server on
+
+// Smartthings Hub Information
+IPAddress hubIp(192, 168, 1, 104);  // smartthings hub ip       //  <---You must edit this line!
+const unsigned int hubPort = 39500; // smartthings hub port
+
+// Hubitat Hub Information
+//IPAddress hubIp(192, 168, 1, 143);    // hubitat hub ip         //  <---You must edit this line!
+//const unsigned int hubPort = 39501;   // hubitat hub port
+
+//******************************************************************************************
+//st::Everything::callOnMsgSend() optional callback routine.  This is a sniffer to monitor 
+//    data being sent to ST.  This allows a user to act on data changes locally within the 
+//    Arduino sktech.
+//******************************************************************************************
+void callback(const String &msg)
+{
+  Serial.print(F("ST_Anything Callback: Sniffed data = "));
+  Serial.println(msg);
+  
+  //TODO:  Add local logic here to take action when a device's value/state is changed
+  
+  //Masquerade as the ThingShield to send data to the Arduino, as if from the ST Cloud (uncomment and edit following line)
+  //st::receiveSmartString("Put your command here!");  //use same strings that the Device Handler would send
+}
+
+//******************************************************************************************
+//Arduino Setup() routine
+//******************************************************************************************
+void setup()
+{
+  //******************************************************************************************
+  //Declare each Device that is attached to the Arduino
+  //  Notes: - For each device, there is typically a corresponding "tile" defined in your 
+  //           SmartThings Device Hanlder Groovy code, except when using new COMPOSITE Device Handler
+  //         - For details on each device's constructor arguments below, please refer to the 
+  //           corresponding header (.h) and program (.cpp) files.
+  //         - The name assigned to each device (1st argument below) must match the Groovy
+  //           Device Handler names.  (Note: "temphumid" below is the exception to this rule
+  //           as the DHT sensors produce both "temperature" and "humidity".  Data from that
+  //           particular sensor is sent to the ST Hub in two separate updates, one for 
+  //           "temperature" and one for "humidity")
+  //         - The new Composite Device Handler is comprised of a Parent DH and various Child
+  //           DH's.  The names used below MUST not be changed for the Automatic Creation of
+  //           child devices to work properly.  Simply increment the number by +1 for each duplicate
+  //           device (e.g. contact1, contact2, contact3, etc...)  You can rename the Child Devices
+  //           to match your specific use case in the ST Phone Application.
+  //******************************************************************************************
+  //Polling Sensors
+  
+  //Interrupt Sensors 
+
+  //Special sensors/executors (uses portions of both polling and executor classes)
+  
+  //Executors
+  static st::EX_Switch executor1(F("switch1"), PIN_S1, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor2(F("switch2"), PIN_S2, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor3(F("switch3"), PIN_S3, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor4(F("switch4"), PIN_S4, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor5(F("switch5"), PIN_S5, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor6(F("switch6"), PIN_S6, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor7(F("switch7"), PIN_S7, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  static st::EX_Switch executor8(F("switch8"), PIN_S8, LOW, false);  //Non-Inverted logic for "Active High" Relay Board
+  
+  //*****************************************************************************
+  //  Configure debug print output from each main class 
+  //  -Note: Set these to "false" if using Hardware Serial on pins 0 & 1
+  //         to prevent communication conflicts with the ST Shield communications
+  //*****************************************************************************
+  st::Everything::debug=true;
+  st::Executor::debug=true;
+  st::Device::debug=true;
+  st::PollingSensor::debug=true;
+  st::InterruptSensor::debug=true;
+
+  //*****************************************************************************
+  //Initialize the "Everything" Class
+  //*****************************************************************************
+
+  //Initialize the optional local callback routine (safe to comment out if not desired)
+  st::Everything::callOnMsgSend = callback;
+  
+  //Create the SmartThings ESP8266WiFi Communications Object
+    //STATIC IP Assignment - Recommended
+    st::Everything::SmartThing = new st::SmartThingsESP8266WiFi(str_ssid, str_password, ip, gateway, subnet, dnsserver, serverPort, hubIp, hubPort, st::receiveSmartString, "LinkNodeR8");
+ 
+    //DHCP IP Assigment - Must set your router's DHCP server to provice a static IP address for this device's MAC address
+    //st::Everything::SmartThing = new st::SmartThingsESP8266WiFi(str_ssid, str_password, serverPort, hubIp, hubPort, st::receiveSmartString);
+
+  //Run the Everything class' init() routine which establishes WiFi communications with SmartThings Hub
+  st::Everything::init();
+  
+  //*****************************************************************************
+  //Add each sensor to the "Everything" Class
+  //*****************************************************************************
+      
+  //*****************************************************************************
+  //Add each executor to the "Everything" Class
+  //*****************************************************************************
+  st::Everything::addExecutor(&executor1);
+  st::Everything::addExecutor(&executor2);
+  st::Everything::addExecutor(&executor3);
+  st::Everything::addExecutor(&executor4);
+  st::Everything::addExecutor(&executor5);
+  st::Everything::addExecutor(&executor6);
+  st::Everything::addExecutor(&executor7);
+  st::Everything::addExecutor(&executor8);
+    
+  //*****************************************************************************
+  //Initialize each of the devices which were added to the Everything Class
+  //*****************************************************************************
+  st::Everything::initDevices();
+}
+
+//******************************************************************************************
+//Arduino Loop() routine
+//******************************************************************************************
+void loop()
+{
+  //*****************************************************************************
+  //Execute the Everything run method which takes care of "Everything"
+  //*****************************************************************************
+  st::Everything::run();
+}
