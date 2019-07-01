83cc078af8a0f717d778756cd6950e1ba61a6b6b
ogiewon <danogorchock@yahoo.com>
2019-07-01 17:38:59 -0400

    Update child-relay-switch.groovy

--- HubDuino/Drivers/child-relay-switch.groovy
+++ HubDuino/Drivers/child-relay-switch.groovy
@@ -22,11 +22,12 @@
  *    2017-08-23  Allan (vseven) Added a generateEvent routine that gets info from the parent device.  This routine runs each time the value is updated which can lead to other modifications of the device.
  *    2018-06-02  Dan Ogorchock  Revised/Simplified for Hubitat Composite Driver Model
  *    2018-09-22  Dan Ogorchock  Added preference for debug logging
+ *    2019-07-01  Dan Ogorchock  Added importUrl
  *
  * 
  */
 metadata {
-	definition (name: "Child Relay Switch", namespace: "ogiewon", author: "Dan Ogorchock") {
+	definition (name: "Child Relay Switch", namespace: "ogiewon", author: "Dan Ogorchock", importUrl: "https://raw.githubusercontent.com/DanielOgorchock/ST_Anything/master/HubDuino/Drivers/child-relay-switch.groovy") {
 		capability "Switch"
 		capability "Relay Switch"
 		capability "Actuator"
